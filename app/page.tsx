16e3d2293328a8693eccaf0f1de7f2cdad5b31b4
andreipatza <andreipatza@gmail.com>
2024-11-14 23:59:26 +0200

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -171,6 +171,8 @@ export default function Component() {
     { name: "Jerry's Pizza", src: "/logos/jerryspizza.png" },
     { name: 'Pilot', src: "/logos/pilot.png" },
     { name: 'Salvati Copii', src: "/logos/salvaticopii.png" },
+    { name: 'sweat', src: "/logos/sweat.png" },
+    { name: 'SOLO', src: "/logos/solo.png" },
     { name: 'I Love Failure', src: "/logos/ilovefailure.png" },
     { name: 'Kinetic', src: "/logos/kinetic.png" },
     { name: 'Exavet', src: "/logos/exavet.png" },
@@ -180,12 +182,13 @@ export default function Component() {
     { name: 'ADNV', src: "/logos/adnv.png" },
     { name: 'Createrra', src: "/logos/createrra.png" },
     { name: 'BusinessPeople', src: "/logos/businessPeople.png" },
+    { name: 'nomadia', src: "/logos/nomadia.png" },
+    { name: 'pddle', src: "/logos/pddle.png" },
     { name: 'energyPal', src: "/logos/energyPal.png" },
     { name: 'Bakery School', src: "/logos/bakeryschool.png" },
+    { name: 'skilld', src: "/logos/silkd.png" },
     { name: '11th', src: "/logos/11th.png" },
-    { name: 'pddle', src: "/logos/pddle.png" },
     { name: 'FMS', src: "/logos/fms.png" },
-    { name: 'SOLO', src: "/logos/solo.png" },
     { name: 'esenzio', src: "/logos/esenzio.png" },
   ]
 
@@ -362,7 +365,7 @@ export default function Component() {
                   <Image
                     src={logo.src}
                     alt={logo.name}
-                    width={120}
+                    width={150}
                     height={60}
                     className="max-w-full h-auto filter"
                   />
