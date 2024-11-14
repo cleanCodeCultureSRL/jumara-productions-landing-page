0ef980f5f98b7b882d2cc37eccae062082dc291e
andreipatza <andreipatza@gmail.com>
2024-11-14 16:59:23 +0200

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 'use client'
 
-import { useState, useRef, useEffect } from 'react'
-import Link from 'next/link'
+import { Cog, Eye, Facebook, Film, Instagram, Lightbulb, Rocket, Telescope, Users, Video } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { Instagram, Facebook, Twitter, Linkedin, Mail, FileText, Lightbulb, Figma, CpuIcon, CheckIcon, Telescope, Video, Cog, Rocket, Eye, Film, Users } from 'lucide-react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function LandingPage() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const [formStatus, setFormStatus] = useState('idle')
   const [snackbar, setSnackbar] = useState(null)
   const pathname = usePathname()
-  const servicesRef = useRef(null)
+
   const aboutUsRef = useRef(null)
   const howWeWorkRef = useRef(null)
+  const contactRef = useRef(null)
 
   const [isVisible, setIsVisible] = useState(false)
   const clientsRef = useRef(null)
@@ -48,29 +48,20 @@ export default function LandingPage() {
     red: '#e05e3d'
   }
 
-  const scrollToServices = () => {
-    servicesRef.current?.scrollIntoView({ behavior: 'smooth' })
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
   }
 
   const scrollToAboutUs = () => {
     aboutUsRef.current?.scrollIntoView({ behavior: 'smooth' })
   }
 
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
+  const scrollToHowWeWork = () => {
+    howWeWorkRef.current?.scrollIntoView({ behavior: 'smooth' })
   }
 
-  const handleSubmitNewsletter = async (e) => {
-    e.preventDefault()
-    const email = e.target[0].value
-    setFormStatus('submitting')
-
-    // Simulating API call
-    await new Promise(resolve => setTimeout(resolve, 1000))
-
-    setFormStatus('success')
-    setSnackbar({ message: 'Te-ai abonat cu succes la newsletter!', type: 'success' })
-    e.target.reset()
+  const scrollToContact = () => {
+    contactRef.current?.scrollIntoView({ behavior: 'smooth' })
   }
 
   const closeSnackbar = () => setSnackbar(null)
@@ -104,29 +95,25 @@ export default function LandingPage() {
 
   return (
     <div className="min-h-screen flex flex-col relative bg-[#061425]">
-      <header className="bg-transparent absolute top-0 left-0 right-0 z-50">
+      <header className="bg-transparent absolute top-0 left-0 right-0 z-50 sticky">
         <div className="container mx-auto px-4 py-4 flex items-center justify-between max-h-[70px]">
           <Link href="/" className="flex items-center space-x-2">
             <Image src="/logo.png" alt="Jumară Productions Logo" width={80} height={40} />
           </Link>
           <nav className="hidden md:flex items-center space-x-8 h-full py-2">
-            <Link href="/" className={`text-[${colors.white}] hover:text-[${colors.red}] font-bold flex items-center h-full relative group ${pathname === '/' ? `text-[${colors.red}]` : ''}`}>
+            <button onClick={scrollToTop} className={`text-[${colors.white}] hover:text-[${colors.red}] font-bold flex items-center h-full relative group ${pathname === '/' ? `text-[${colors.red}]` : ''}`}>
               ACASĂ
               <span className={`absolute bottom-[-4px] left-0 w-full h-1 bg-[${colors.red}] transform transition-transform duration-300 ease-in-out ${pathname === '/' ? 'scale-x-100' : 'scale-x-0 group-hover:scale-x-100'}`} />
-            </Link>
-            <Link href="/about" className={`text-[${colors.white}] hover:text-[${colors.red}] font-bold flex items-center h-full relative group ${pathname === '/about' ? `text-[${colors.red}]` : ''}`}>
+            </button>
+            <button onClick={scrollToAboutUs} className={`text-[${colors.white}] hover:text-[${colors.red}] font-bold flex items-center h-full relative group ${pathname === '/about' ? `text-[${colors.red}]` : ''}`}>
               DESPRE NOI
               <span className={`absolute bottom-[-4px] left-0 w-full h-1 bg-[${colors.red}] transform transition-transform duration-300 ease-in-out ${pathname === '/about' ? 'scale-x-100' : 'scale-x-0 group-hover:scale-x-100'}`} />
-            </Link>
-            <Link href="/services" className={`text-[${colors.white}] hover:text-[${colors.red}] font-bold flex items-center h-full relative group ${pathname === '/services' ? `text-[${colors.red}]` : ''}`}>
-              SERVICII
-              <span className={`absolute bottom-[-4px] left-0 w-full h-1 bg-[${colors.red}] transform transition-transform duration-300 ease-in-out ${pathname === '/services' ? 'scale-x-100' : 'scale-x-0 group-hover:scale-x-100'}`} />
-            </Link>
-            <Link href="/work" className={`text-[${colors.white}] hover:text-[${colors.red}] font-bold flex items-center h-full relative group ${pathname === '/work' ? `text-[${colors.red}]` : ''}`}>
+            </button>
+            <button onClick={scrollToHowWeWork} className={`text-[${colors.white}] hover:text-[${colors.red}] font-bold flex items-center h-full relative group ${pathname === '/work' ? `text-[${colors.red}]` : ''}`}>
               CUM LUCRĂM
               <span className={`absolute bottom-[-4px] left-0 w-full h-1 bg-[${colors.red}] transform transition-transform duration-300 ease-in-out ${pathname === '/work' ? 'scale-x-100' : 'scale-x-0 group-hover:scale-x-100'}`} />
-            </Link>
-            <Link href="/contact" className={`bg-[${colors.red}] text-[${colors.white}] px-4 py-2 font-bold rounded hover:bg-opacity-90 flex items-center h-[calc(100%-16px)] my-2`}>CONTACT</Link>
+            </button>
+            <button onClick={scrollToContact} className={`bg-[${colors.red}] text-[${colors.white}] px-4 py-2 font-bold rounded hover:bg-opacity-90 flex items-center h-[calc(100%-16px)] my-2`}>CONTACT</button>
           </nav>
           <button
             className="md:hidden p-2 text-[${colors.white}]"
@@ -150,7 +137,7 @@ export default function LandingPage() {
 
       <main className="flex-grow z-10 relative">
         {/* HERO SECTION */}
-        <section className="relative h-screen">
+        <section className="relative h-screen" style={{ marginTop: "-70px" }}>
           <video
             className="absolute top-0 left-0 w-full h-full object-cover"
             autoPlay
@@ -168,7 +155,7 @@ export default function LandingPage() {
         {/* BANDA TAGLINE */}
         <section className={`bg-[#34353a] text-[${colors.white}] py-16`}>
           <div className="container mx-auto px-4">
-            <h3 className="text-2xl md:text-3xl font-bold mb-4">
+            <h3 className="text-2xl md:text-3xl font-bold mb-4 font-monumentRegular">
               JUMARĂ PRODUCTIONS. <span className={`text-[${colors.red}]`}>CONTENT VIDEO AGENCY.</span>
             </h3>
             <h2 className="text-4xl md:text-5xl lg:text-6xl font-extrabold">
@@ -178,7 +165,7 @@ export default function LandingPage() {
         </section>
 
         {/* STATS SECTION */}
-        <section className="bg-[#34353a] py-16 relative overflow-hidden">
+        <section ref={aboutUsRef} className="bg-[#34353a] py-16 relative overflow-hidden">
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap justify-center items-stretch -mx-4">
               <div className="w-full sm:w-1/3 px-4 mb-8 sm:mb-0">
@@ -219,7 +206,7 @@ export default function LandingPage() {
         {/* CLIENTI */}
         <section ref={clientsRef} className="py-16 bg-white pt-40">
           <div className="container mx-auto px-4 mt-50" >
-            <h2 className="text-3xl md:text-4xl font-bold mb-12 text-black text-center">CLIENȚII NOȘTRII</h2>
+            <h2 className="text-3xl md:text-4xl font-bold mb-12 text-black text-center font-archivo">CLIENȚII NOȘTRII</h2>
             <p className="text-xl text-center text-gray-600 mb-8">
               Lucrăm de la start-up-uri până la retaileri și branduri recunoscute la nivel global
             </p>
@@ -247,7 +234,7 @@ export default function LandingPage() {
         </section>
 
         {/* CUM LUCRAM */}
-        <section id="howWeWork" className="w-full bg-[#34353a] py-20" ref={howWeWorkRef} >
+        <section ref={howWeWorkRef} id="howWeWork" className="w-full bg-[#34353a] py-20" >
           <div className="container mx-auto px-8">
             <div className="text-center mb-12">
               <h2 className="text-lg text-[#e05e3d] mb-4 select-none">CUM FUNCȚIONEAZĂ</h2>
@@ -280,7 +267,7 @@ export default function LandingPage() {
         </section>
 
         {/* CONTACT SECTION */}
-        <section className="bg-white py-16 relative overflow-hidden">
+        <section ref={contactRef} className="bg-white py-16 relative overflow-hidden">
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap -mx-4">
               <div className="w-full lg:w-1/4 px-4 mb-8 lg:mb-0">
@@ -332,7 +319,7 @@ export default function LandingPage() {
               </div>
             </div>
           </div>
-          <div className="absolute bottom-0 right-0 w-1/3 h-1/3 bg-[#e05e3d] rounded-tl-full opacity-20"></div>
+          <div className="absolute bottom-0 right-0 w-1/3 h-1/6 bg-[#e05e3d] rounded-tl-full opacity-20"></div>
         </section>
       </main>
 
